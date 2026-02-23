c19087f3b067e92f6c5c2a249ac2bd1935cbf136
nipun-munasinghe <nipunlakmal884@gmail.com>
2026-02-23 19:57:03 +0530

    Add promises example

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => { 
-    data += chunk; 
-  });
-
-  resp.on('end', () => { 
-    console.log(JSON.parse(data)); 
-  });
-}).on('error', (err) => { 
-    console.log('Error:', err.message); 
+const myPromise = new Promise((resolve, reject) => {
+  const condition = true;
+  if (condition) resolve('Success!');
+  else reject('Failure!');
 });
+myPromise.then(result => console.log(result))
+         .catch(error => console.log(error));
